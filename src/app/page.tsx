8842d8ba2ab8dcc4d413f14f2ecd177f8c59c12a
Kyle Greene <kmgreene@gmail.com>
2024-07-10 10:50:51 -0700

    Adjusted the formatting of the page into a list and changed date formatting.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 interface Guest {
   id: string;
   guest: string;
-  created_at: string;
+  created_at: Date;
 }
 
 const Home = () => {
@@ -16,7 +16,13 @@ const Home = () => {
       try {
         const response = await fetch("/api/guest");
         const data: Guest[] = await response.json();
-        setGuests(data);
+
+        const guestsWithFormattedDates = data.map((guest) => ({
+          ...guest,
+          created_at: new Date(guest.created_at),
+        }));
+
+        setGuests(guestsWithFormattedDates);
       } catch (e) {
         console.error(e);
       }
@@ -27,18 +33,19 @@ const Home = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        {guests.map((guest) => (
-          <p
-            key={guest.id}
-            className="m-0 max-w-[30ch] text-balance text-sm opacity-50"
-          >
-            <div className="mb-4">{guest.id}</div>
-
-            <div className="mb-4">{guest.guest}</div>
-            <div className="mb-4">{guest.created_at}</div>
-          </p>
-        ))}
+      <div className="max-w-5xl mx-auto">
+        <ul className="divide-y divide-gray-200">
+          {guests.map((guest) => (
+            <li key={guest.id} className="py-4">
+              <div className="flex items-center justify-between">
+                <div className="text-lg font-semibold">{guest.guest}</div>
+                <div className="ml-4 text-sm text-gray-500">
+                  {guest.created_at.toLocaleString()}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
