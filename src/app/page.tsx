40f970bccc4b673ae3d55232940fd6dfd5d85b32
Kyle Greene <kmgreene@gmail.com>
2024-07-11 14:34:06 -0700

    Added dark mode settings for form field.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,7 @@ const Home = () => {
           type="text"
           value={newGuestName}
           onChange={(e) => setNewGuestName(e.target.value)}
-          className="py-2 px-4 mr-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+          className="py-2 px-4 mr-2 border border-gray-300 dark:border-gray-600 rounded-md focus:outline-none focus:border-blue-500 dark:bg-gray-800 dark:text-gray-300"
           placeholder="Enter your name here."
           required
         ></input>
